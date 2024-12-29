65eb6cc0f65b7c66972c14fbe8e998cbe7166d1d
Weiren Gan <weirengan@WeirendeMacBook-Pro.local>
2024-12-29 21:03:47 +0800

    add: something bit

---
+++ src/components/common/Container.tsx
@@ -0,0 +1,30 @@
+import { forwardRef } from "react"
+import { ComponentType } from "@/types/common"
+import classNames from "classnames"
+
+interface ContainerItf extends ComponentType {
+    type: "page" | "card" | "section"
+}
+
+const Container = forwardRef<HTMLDivElement, ContainerItf>((props, ref) => {
+
+    const {
+        type, 
+        className, style, children,
+    } = props
+
+    const classes = classNames(
+        className,
+        `"container-type_${type}"`,
+    )
+
+    const innerStyle = Object.assign({}, style)
+
+    return (
+        <div ref={ref} className={classes} style={innerStyle}>
+            { children }
+        </div>
+    )
+})
+
+export default Container
---
+++ src/types/common.ts
@@ -0,0 +1,7 @@
+import { CSSProperties, ReactNode } from "react"
+
+export type ComponentType = {
+    children: ReactNode
+    className: string
+    style: CSSProperties
+}
